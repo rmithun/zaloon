0eca2ae86d8336e8d64640ab844137311fcc55e4
Desk-Mithun <vbnetmithun@gmail.com>
2015-06-10 09:25:28 +0530

    changes in settings.py

--- utils/ custom_storages.py
+++
@@ -1,7 +0,0 @@
-###custom function
-
-from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
-
-class StaticStorage(S3BotoStorage):
-    location = settings.STATICFILES_LOCATION
