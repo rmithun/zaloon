f3a7f88736dcc1fd5db3036c672996205fd2ddb6
Jawahar <jawahar7.ceg@gmail.com>
2015-09-30 02:02:07 +0530

    css issues

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 var secretEmptyKey = '[$empty$]'
-var noqapp = angular.module('accountApp', ['ngAnimate','ngCookies','ngRoute','ui.bootstrap','ngLodash','ui.editable','ngTouch','ui.bootstrap.setNgAnimate']);
+var noqapp = angular.module('accountApp', ['ngAnimate','ngCookies','ngRoute','ui.bootstrap','ngLodash','ui.editable','ngTouch','ui.bootstrap.setNgAnimate', 'am.resetField']);
 
 noqapp.run(function($http,$cookies,sessionService) {
 
@@ -135,3 +135,42 @@ noqapp.directive('emptyTypeahead', function () {
         }
       }
     });
+
+angular.module('am.resetField', []).directive('amResetField', ['$compile', '$timeout', function($compile, $timeout) {
+  return {
+    require: 'ngModel',
+    scope: {},
+    link: function(scope, el, attrs, ctrl) {
+      // limit to input element of specific types
+      var inputTypes = /text|search|tel|url|email|password/i;
+      if (el[0].nodeName !== "INPUT")
+        throw new Error("resetField is limited to input elements");
+      if (!inputTypes.test(attrs.type))
+        throw new Error("Invalid input type for resetField: " + attrs.type);
+
+      // compiled reset icon template
+      var template = $compile('<i ng-show="enabled" ng-mousedown="reset()" class="fa fa-times"></i>')(scope);
+      el.after(template);
+
+      scope.reset = function() {
+        ctrl.$setViewValue(null);
+        ctrl.$render();
+        $timeout(function() {
+            el[0].focus();
+        }, 0, false);
+      };
+
+      el.bind('input', function() {
+        scope.enabled = !ctrl.$isEmpty(el.val());
+      })
+      .bind('focus', function() {
+        scope.enabled = !ctrl.$isEmpty(el.val());
+        scope.$apply();
+      })
+      .bind('blur', function() {
+        scope.enabled = true;
+        scope.$apply();
+      });
+    }
+  };
+}]);
